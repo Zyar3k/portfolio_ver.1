a9f3d34b72ff356d2cf47680816ab62ac343c44a
Arkadiusz Zygan <a.zygan@gmail.com>
2020-04-27 20:29:45 +0200

    Add footer

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Footer.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 class Footer extends React.Component {
 
@@ -7,7 +10,20 @@ class Footer extends React.Component {
 
     return(
       <div className='footer'>
-        <h1> Footer</h1>
+        <div className='footDesktop'>
+          <span>copyright Zyar3k - 2020</span>
+          <span>
+            <a href='https://www.linkedin.com/in/arkadiusz-zygan/' target="_blank" rel="noopener noreferrer" className='contactBox'>
+            <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+            <a href='https://github.com/Zyar3k' target="_blank" rel="noopener noreferrer" className='contactBox'>
+            <FontAwesomeIcon icon={faGithub} />
+            </a>
+            <a href='mailto:a.zygan@gmail.com' target='_blank' rel='noopener noreferrer'>
+            <FontAwesomeIcon icon={faEnvelope} />
+            </a>
+          </span>
+        </div>
       </div>
     );
   };
