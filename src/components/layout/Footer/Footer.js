ed87b21c32240ad46206feebfccde39a7c4de8ca
Arkadiusz Zygan <a.zygan@gmail.com>
2020-04-28 21:32:52 +0200

    Fix footer, add rwd

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Footer.scss';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
@@ -10,9 +11,11 @@ class Footer extends React.Component {
 
     return(
       <div className='footer'>
+
         <div className='footDesktop'>
-          <span>copyright Zyar3k - 2020</span>
-          <span>
+          <div className='leftFoot'>copyright Zyar3k - 2020</div>
+          
+          <div className='rightFoot'>
             <a href='https://www.linkedin.com/in/arkadiusz-zygan/' target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faLinkedin} />
             </a>
@@ -22,7 +25,12 @@ class Footer extends React.Component {
             <a href='mailto:a.zygan@gmail.com' target='_blank' rel='noopener noreferrer'>
             <FontAwesomeIcon icon={faEnvelope} />
             </a>
-          </span>
+          </div>
+        </div>
+        
+        <div className='footMobile'>
+          <Link to='/portfolio'>Portfolio</Link>
+          <Link to='/contact'>Kontakt</Link>
         </div>
       </div>
     );
