db3ee5f82e7ab38eb3b415b87f0f890137bb7f17
Arkadiusz Zygan <a.zygan@gmail.com>
2020-04-29 14:29:30 +0200

    Updates

---
+++ precache-manifest.ad12e3c8265e378d4b17aafa2994d6ae.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f7fc5978ae125939af7961dd6ad6e70",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c6fa184517585bc8543b",
+    "url": "./static/css/main.ed174438.chunk.css"
+  },
+  {
+    "revision": "4d563375930fe1b251fc",
+    "url": "./static/js/2.4c5b0118.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.4c5b0118.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6fa184517585bc8543b",
+    "url": "./static/js/main.a931de15.chunk.js"
+  },
+  {
+    "revision": "d7ca1d6ae5c0fe49a6a6",
+    "url": "./static/js/runtime-main.a43d3f6e.js"
+  },
+  {
+    "revision": "b6a97c7c9d3af27e246c33405cf5c675",
+    "url": "./static/media/bcd.b6a97c7c.png"
+  },
+  {
+    "revision": "624014eb35043c3fa3f92c032f3818f9",
+    "url": "./static/media/group_project.624014eb.png"
+  },
+  {
+    "revision": "5fc475f178ea14a784ae0de990fbdd38",
+    "url": "./static/media/head.5fc475f1.jpg"
+  },
+  {
+    "revision": "260fca5990262abdb2ecda847d3cdbbc",
+    "url": "./static/media/new_wave.260fca59.png"
+  },
+  {
+    "revision": "50432958f96ff4d780d0567df9f8de86",
+    "url": "./static/media/panel_adm.50432958.png"
+  },
+  {
+    "revision": "e7017523c1910f390be4c019844d16fa",
+    "url": "./static/media/rps.e7017523.png"
+  },
+  {
+    "revision": "4f4d9be62480ecc01813ce3c775757f0",
+    "url": "./static/media/weather.4f4d9be6.png"
+  }
+]);
