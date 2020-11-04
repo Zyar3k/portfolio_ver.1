a96b752ff316d5247d31bff8570d6edd1f20f700
Arkadiusz Zygan <a.zygan@gmail.com>
2020-11-04 11:23:20 +0100

    Edit portfolio, add const db

--- src/components/views/Portfolio/Portfolio.js
+++ src/components/views/Portfolio/Portfolio.js
@@ -1,139 +1,148 @@
-import React from 'react';
-import './Portfolio.scss';
-import PageTitle from '../../common/PageTitle/PageTitle';
-import img1 from './bcd.png';
-import img2 from './panel_adm.png';
-import img3 from './rps.png';
-import img4 from './local_weather.jpg';
-import img5 from './group_project.png';
-import img6 from './new_wave.png';
-import img7 from './czatap_port.png';
-import img8 from './Covid-19 Counter.png';
-import img9 from './Numerologia.png';
-import img10 from './Picture_shop.png';
+import React from "react";
+import "./Portfolio.scss";
+import PageTitle from "../../common/PageTitle/PageTitle";
+// import img1 from "./bcd.png";
+import img1 from "../../../images/bcd.png";
+import img2 from "../../../images/panel_adm.png";
+import img3 from "../../../images/rps.png";
+import img4 from "../../../images/local_weather.jpg";
+import img5 from "../../../images/group_project.png";
+import img6 from "../../../images/new_wave.png";
+import img7 from "../../../images/czatap_port.png";
+import img8 from "../../../images/Covid-19 Counter.png";
+import img9 from "../../../images/Numerologia.png";
+import img10 from "../../../images/Picture_shop.png";
+import img11 from "../../../images/toDoApp.png";
 
-class Portfolio extends React.Component {
+const portfolioDB = {
+  title: "portfolio",
+  projects: [
+    {
+      name: "Betty Craft Designs",
+      img: img1,
+      code: "https://github.com/Zyar3k/online_shop_bcd",
+      demo: "https://online-shop-bcd.herokuapp.com/",
+      desc:
+        "Indywidualny projekt na zakończenie Bootcampu Web Developer Plus. Tech stack: MERN - MongoDB, Express, React (Redux, Router), Node.js | Sass | CRA - webpack | npm | Yarn.",
+    },
+    {
+      name: "Cz@-App",
+      img: img7,
+      code: "https://github.com/Zyar3k/your_chat_app/tree/master",
+      demo: "https://youthful-clarke-0b8301.netlify.app/",
+      desc:
+        "Aplikacja czatowa w czasie rzeczywistym Tech stack: HTML | Sass | RWD | React | Express | Node | Socket.IO | CRA- webpack | git | Yarn.",
+    },
+    {
+      name: "statystyki CovID-19",
+      img: img8,
+      code: "https://github.com/Zyar3k/covid-19_counter",
+      demo: "https://covid-19-counter.netlify.app/",
+      desc:
+        "Aplikacja pozwalająca śledzić na bieżąco statystyki dotyczące Covid-19. Tech stack: HTML | Sass | RWD | React | API | npm Scripts.",
+    },
+    {
+      name: "kamień - papier - nożyce",
+      img: img3,
+      code: "https://github.com/Zyar3k/rock-paper-scissors",
+      demo: "https://zyar3k.github.io/rock-paper-scissors/",
+      desc:
+        "Pierwsza gra napisana w JavaScript wyświetlająca wynik rozgrywki. Tech stack: HTML | Sass | RWD | JavaScript ES5 | npm Scripts.",
+    },
+    {
+      name: "numerologia",
+      img: img9,
+      code: "https://github.com/Zyar3k/numerology_4.0.0",
+      demo: "https://numerologia.netlify.app/",
+      desc:
+        "Aplikacja pozwalająca poznać swoje specjalne liczby i sprawdzić wibracje numerologiczne na dwoolny dzień/miesiąc. Tech stack: HTML | Sass | RWD | JavaScript ES5 | npm Scripts.",
+    },
+    {
+      name: "picture shop",
+      img: img10,
+      code: "https://github.com/Zyar3k/picture_shop",
+      demo: "https://picture-shop.netlify.app/",
+      desc:
+        "Strona z elementami portfolio fotografa. Działający koszyk i zwalidowany formularz wiadomości. Tech stack: HTML | Sass | RWD | JavaScript ES5 | npm Scripts.",
+    },
+    {
+      name: "Aplikacja pogodowa",
+      img: img4,
+      code: "https://github.com/Zyar3k/local_weather_app",
+      demo: "https://agitated-aryabhata-cef99e.netlify.app/",
+      desc:
+        "Aplikacja pogodowa z użyciem API - wyświetlająca aktualną pogodę w szukanym mieście. Tech stack: HTML | Sass | RWD | React | API | CRA- webpack | Yarn.",
+    },
+    {
+      name: "panel administracyjny",
+      img: img2,
+      code: "https://github.com/Zyar3k/Solo-project",
+      demo: "https://zyar3k.github.io/Solo-project/#",
+      desc:
+        "Pierwszy indywidualny projekt napisany podczas Bootcampu Web Developer Plus na podstawie pliku PSD od grafika. Tech stack: HTML | Sass | RWD | JavaScript ES5 | Photoshop | npm Scripts.",
+    },
+    {
+      name: "Projekt grupowy",
+      img: img5,
+      code: "https://github.com/Zyar3k/kodilla_group_project",
+      demo: "https://stupefied-lewin-977cb1.netlify.com/",
+      desc:
+        "Projekt grupowy - symulacja pracy w grupie z innymi junior Front-end developerami. Każdy z uczestników wybierał/miał przydzielane zadania. Tech stack: HTML | Sass | RWD | React | API | CRA- webpack | git | Jira | kanban | Yarn.",
+    },
+    {
+      name: "Do zrobienia!",
+      img: img11,
+      code: "https://github.com/Zyar3k/todo_react_app",
+      demo: "https://dozrobienia-app.netlify.app/",
+      desc:
+        "Aplikacja ToDo - aplikacja stworzona w React. Priorytety i ukończone zadania. Tech stack: HTML | Sass | RWD | React | CRA- webpack | git | Yarn.",
+    },
+    {
+      name: "New Wave festival",
+      img: img6,
+      code: "https://github.com/Zyar3k/newwavefestival-feb2020",
+      demo: "https://secure-woodland-63361.herokuapp.com/",
+      desc:
+        "Projekt treningowy - praca skupiona na backendowej stronie projektu oraz połączenie front i backendu. Tech stack: HTML | Sass | RWD | React | API | CRA- webpack | git | Yarn.",
+    },
+  ],
+};
 
-  render(){
-
-    return(
-      <div className='pageContent'>
-        <PageTitle>portfolio</PageTitle>
-        <div className='portfolio'>
-          <div className='portfolioBox'>
-            <p className='portfolioTitle'>Betty Craft Designs</p>
-            <div className='imgWrapper'>
-              <img src={img1} alt='img' />
-              <div className='portfolioBtns'>
-                <a href='https://github.com/Zyar3k/online_shop_bcd' className='portfolioBtn'>kod</a>
-                <a href='https://online-shop-bcd.herokuapp.com/' className='portfolioBtn'>demo</a>
-              </div>
-            </div>
-            <p className='portfolioDesc'>Indywidualny projekt na zakończenie Bootcampu Web Developer Plus. Tech stack: MERN - MongoDB, Express, React (Redux, Router), Node.js | Sass | CRA - webpack | npm | Yarn.</p>
-          </div>
-          <div className='portfolioBox'>
-            <p className='portfolioTitle'>Cz@-App</p>
-            <div className='imgWrapper'>
-              <img src={img7} alt='img' />
-              <div className='portfolioBtns'>
-                <a href='https://github.com/Zyar3k/your_chat_app/tree/master' className='portfolioBtn'>kod</a>
-                <a href='https://youthful-clarke-0b8301.netlify.app/' className='portfolioBtn'>demo</a>
-              </div>
-            </div>
-            <p className='portfolioDesc'>Aplikacja czatowa w czasie rzeczywistym  Tech stack: HTML | Sass | RWD | React | Express | Node | Socket.IO | CRA- webpack | git | Yarn.</p>
-          </div>
-          <div className='portfolioBox'>
-            <p className='portfolioTitle'>statystyki CovID-19</p>
-            <div className='imgWrapper'>
-              <img src={img8} alt='img' />
-              <div className='portfolioBtns'>
-                <a href='https://github.com/Zyar3k/covid-19_counter' className='portfolioBtn'>kod</a>
-                <a href='https://covid-19-counter.netlify.app/' target='_blank' rel='noopener noreferrer' className='portfolioBtn'>demo</a>
-              </div>
-            </div>
-            <p className='portfolioDesc'>Aplikacja pozwalająca śledzić na bieżąco statystyki dotyczące Covid-19. Tech stack: HTML | Sass | RWD | React | API | npm Scripts.</p>
-          </div>          
-          <div className='portfolioBox'>
-            <p className='portfolioTitle'>kamień - papier - nożyce</p>
-            <div className='imgWrapper'>
-              <img src={img3} alt='img' />
-              <div className='portfolioBtns'>
-                <a href='https://github.com/Zyar3k/rock-paper-scissors' className='portfolioBtn'>kod</a>
-                <a href='https://zyar3k.github.io/rock-paper-scissors/' className='portfolioBtn'>demo</a>
-              </div>
-            </div>
-            <p className='portfolioDesc'>Pierwsza gra napisana w JavaScript wyświetlająca wynik rozgrywki. Tech stack: HTML | Sass | RWD | JavaScript ES5 | npm Scripts.</p>
-          </div>
-          <div className='portfolioBox'>
-            <p className='portfolioTitle'>numerologia</p>
-            <div className='imgWrapper'>
-              <img src={img9} alt='img' />
-              <div className='portfolioBtns'>
-                <a href='https://github.com/Zyar3k/numerology_4.0.0' className='portfolioBtn'>kod</a>
-                <a href='https://numerologia.netlify.app/' className='portfolioBtn'>demo</a>
-              </div>
-            </div>
-            <p className='portfolioDesc'>Aplikacja pozwalająca poznać swoje specjalne liczby i sprawdzić wibracje numerologiczne na dwoolny dzień/miesiąc. Tech stack: HTML | Sass | RWD | JavaScript ES5 | npm Scripts.</p>
-          </div>
-          <div className='portfolioBox'>
-            <p className='portfolioTitle'>picture shop</p>
-            <div className='imgWrapper'>
-              <img src={img10} alt='img' />
-              <div className='portfolioBtns'>
-                <a href='https://github.com/Zyar3k/picture_shop' className='portfolioBtn'>kod</a>
-                <a href='https://picture-shop.netlify.app/' className='portfolioBtn'>demo</a>
-              </div>
-            </div>
-            <p className='portfolioDesc'>Strona z elementami portfolio fotografa. Działający koszyk i zwalidowany formularz wiadomości. Tech stack: HTML | Sass | RWD | JavaScript ES5 | npm Scripts.</p>
-          </div>
-          <div className='portfolioBox'>
-            <p className='portfolioTitle'>Aplikacja pogodowa</p>
-            <div className='imgWrapper'>
-              <img src={img4} alt='img' />
-              <div className='portfolioBtns'>
-                <a href='https://github.com/Zyar3k/local_weather_app' className='portfolioBtn'>kod</a>
-                <a href='https://agitated-aryabhata-cef99e.netlify.app/' className='portfolioBtn'>demo</a>
-              </div>
-            </div>
-            <p className='portfolioDesc'>Aplikacja pogodowa z użyciem API - wyświetlająca aktualną pogodę w szukanym mieście. Tech stack: HTML | Sass | RWD | React | API | CRA- webpack | Yarn.</p>
-          </div>
-          <div className='portfolioBox'>
-            <p className='portfolioTitle'>panel administracyjny</p>
-            <div className='imgWrapper'>
-              <img src={img2} alt='img' />
-              <div className='portfolioBtns'>
-                <a href='https://github.com/Zyar3k/Solo-project' className='portfolioBtn'>kod</a>
-                <a href='https://zyar3k.github.io/Solo-project/#' target='_blank' rel='noopener noreferrer' className='portfolioBtn'>demo</a>
-              </div>
-            </div>
-            <p className='portfolioDesc'>Pierwszy indywidualny projekt napisany podczas Bootcampu Web Developer Plus na podstawie pliku PSD od grafika. Tech stack: HTML | Sass | RWD | JavaScript ES5 | Photoshop | npm Scripts.</p>
-          </div>
-          <div className='portfolioBox'>
-            <p className='portfolioTitle'>Projekt grupowy</p>
-            <div className='imgWrapper'>
-              <img src={img5} alt='img' />
-              <div className='portfolioBtns'>
-                <a href='https://github.com/Zyar3k/kodilla_group_project' className='portfolioBtn'>kod</a>
-                <a href='https://stupefied-lewin-977cb1.netlify.com/' className='portfolioBtn'>demo</a>
-              </div>
-            </div>
-            <p className='portfolioDesc'>Projekt grupowy - symulacja pracy w grupie z innymi junior Front-end developerami. Każdy z uczestników wybierał/miał przydzielane zadania.  Tech stack: HTML | Sass | RWD | React | API | CRA- webpack | git | Jira | kanban | Yarn.</p>
-          </div>
-          <div className='portfolioBox'>
-            <p className='portfolioTitle'>New Wave festival</p>
-            <div className='imgWrapper'>
-              <img src={img6} alt='img' />
-              <div className='portfolioBtns'>
-                <a href='https://github.com/Zyar3k/newwavefestival-feb2020' className='portfolioBtn'>kod</a>
-                <a href='https://secure-woodland-63361.herokuapp.com/' className='portfolioBtn'>demo</a>
-              </div>
-            </div>
-            <p className='portfolioDesc'>Projekt treningowy - praca skupiona na backendowej stronie projektu oraz połączenie front i backendu.  Tech stack: HTML | Sass | RWD | React | API | CRA- webpack | git | Yarn.</p>
-          </div>
-        </div>
+const portfolioElements = portfolioDB.projects.map((item, index) => (
+  <div key={index} className='portfolioBox'>
+    <p className='portfolioTitle'>{item.name}</p>
+    <div className='imgWrapper'>
+      <img src={item.img} alt='img' />
+      <div className='portfolioBtns'>
+        <a
+          href={item.code}
+          target='_blank'
+          rel='noopener noreferrer'
+          className='portfolioBtn'
+        >
+          kod
+        </a>
+        <a
+          href={item.demo}
+          target='_blank'
+          rel='noopener noreferrer'
+          className='portfolioBtn'
+        >
+          demo
+        </a>
       </div>
-    );
-  };
+    </div>
+    <p className='portfolioDesc'>{item.desc}</p>
+  </div>
+));
+
+const Portfolio = () => {
+  return (
+    <div className='pageContent'>
+      <PageTitle>{portfolioDB.title}</PageTitle>
+      <div className='portfolio'>{portfolioElements}</div>
+    </div>
+  );
 };
 
 export default Portfolio;
