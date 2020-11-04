8fde6b544c958c46a868fa294b9136faccedbaaf
Arkadiusz Zygan <a.zygan@gmail.com>
2020-11-04 11:53:45 +0100

    Edit home - add const db

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -1,33 +1,39 @@
-import React from 'react';
-import './Homepage.scss';
-import img1 from './head.jpg';
+import React from "react";
+import "./Homepage.scss";
+import img1 from "../../../images/head.jpg";
 
-class Homepage extends React.Component {
+const homeDB = {
+  quote:
+    "Sukces niekoniecznie osiąga najlepszy, tylko ten, kto potrafi otoczyć się najlepszymi",
+  author: "Carla Montero",
+  img: img1,
+  texts: [
+    "chcę się uczyć",
+    "chcę się rozwijać",
+    "wiem, że wiele nauki przede mną",
+    "cel jest drogą ciągłego rozwoju",
+  ],
+};
 
-  render(){
+const texts = homeDB.texts.map((item, index) => <div key={index}>{item}</div>);
 
-    return(
-      <div className='pageContent'>
-        <div className='homepage'>
-          <div className='homeText'>
-            <div className='quotetext'>Sukces niekoniecznie osiąga najlepszy, tylko ten, kto potrafi otoczyć się najlepszymi</div>
-            <div className='quoteAuthor'>Carla Montero</div>
+const Homepage = () => {
+  return (
+    <div className='pageContent'>
+      <div className='homepage'>
+        <div className='homeText'>
+          <div className='quotetext'>{homeDB.quote}</div>
+          <div className='quoteAuthor'>{homeDB.author}</div>
+        </div>
+        <div className='imgHomeWrapper'>
+          <div className='imgHome'>
+            <img src={homeDB.img} alt='img' />
           </div>
-          <div className='imgHomeWrapper'>
-            <div className='imgHome'>
-              <img src={img1} alt='img' />
-            </div>
-            <div className='imgTextWrapper'>
-              <div>chcę się uczyć</div>
-              <div>chcę się rozwijać</div>
-              <div>wiem, że wiele nauki przede mną</div>
-              <div>cel jest drogą ciągłego rozwoju</div>
-            </div>
-          </div>
+          <div className='imgTextWrapper'>{texts}</div>
         </div>
       </div>
-    );
-  };
+    </div>
+  );
 };
 
 export default Homepage;
